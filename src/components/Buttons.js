fd88b6f244b1b8786e105ab501c1eab64fac1dce
Ashkan Valizadeh <ashvalizadeh@gmail.com>
2022-06-04 15:59:21 +0430

    new change

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -4,19 +4,19 @@ import {Link} from 'react-router-dom';
 const Buttons = () => {
     return (
         <div>
-            <div className="flex items-center justify-between gap-x-2 mb-5  ">
+            <div className="flex items-center justify-between gap-x-2 w-full mb-5 overflow-x-auto ">
                 <Link to="/categoryform">
-                <button   className ="flex-1   bg-slate-500 text-slate-200 rounded-xl py-2 px-2">Add
+                <button   className ="flex-1   bg-slate-500 text-slate-200 rounded-xl py-2 px-4 w-full">Add
                 Category
                 </button>
                 </Link>
                 <Link to="/productform">
-                <button   className ="flex-1 bg-slate-500 text-slate-200 rounded-xl py-2 px-3">Add
+                <button   className ="flex-1 bg-slate-500 text-slate-200 rounded-xl py-2 px-5   w-full">Add
                 Product
                 </button>
                 </Link>
                 <Link to="/products">
-                <button  className ="flex-1 bg-slate-500 text-slate-200 rounded-xl py-2 px-6">Products
+                <button  className ="flex-1 bg-slate-500 text-slate-200 rounded-xl py-2 px-3 w-full"> List of Products
                 </button>
                 </Link>
             
